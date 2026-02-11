ea4821d9b94874809a0980eb6a41e844cc24fa1a
maéva neveu <maeva.neveu@ynov.com>
2026-02-11 16:53:10 +0100

    [CHANGE] type req and res

--- server.ts
+++ server.ts
@@ -3,18 +3,20 @@ import express, { Application, Request, Response } from 'express'
 import { pool } from './db.js'
 
 interface User { 
-    id: string; username: string; birthdate?: string
+    id: string
+    username: string
+    birthdate?: string
 }
 
 const app: Application = express()
-const PORT = 4000
+const PORT: number = 4000
 
 const users: User[] = []
 
 app.use(express.json())
 
 // logique métier
-app.get('/users/:id', async (req, res) => {
+app.get('/users/:id', async (req: Request, res: Response) => {
     const id = req.params.id
 
     const [rows]: any = await pool.promise().query(
@@ -26,14 +28,14 @@ app.get('/users/:id', async (req, res) => {
     return res.status(200).json(rows)
 })
 
-app.get('/users/:username', (req, res) => {
-    const { username } = req.params
+app.get('/users/:username', (req: Request, res: Response) => {
+    const username = Array.isArray(req.params.username) ? req.params.username[0] : req.params.username
 
     const result = users.filter((user) => user.username.toLowerCase() === username.toLowerCase())
     return res.json(result)
 })
 
-app.post('/users',async (req, res) => {
+app.post('/users', async (req: Request, res: Response) => {
     const { username, birthdate } = req.body
     pool.promise().execute(
         'INSERT INTO users (username, birthdate) VALUES (?, ?)',
@@ -42,7 +44,7 @@ app.post('/users',async (req, res) => {
     res.status(201).json({ username, birthdate })
 })
 
-app.delete('/users/:id', (req, res) => {
+app.delete('/users/:id', (req: Request, res: Response) => {
     const i = users.findIndex(x => x.id === req.params.id)
     const [d] = users.splice(i, 1)
     res.json(d)
